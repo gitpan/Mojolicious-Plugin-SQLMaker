574bec3c2290f5b34fd7e55b99fe70762040ffdb
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2012-12-05 02:49:58 +0000

    Import of FUKATA/Mojolicious-Plugin-SQLMaker-0.03 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-SQLMaker
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         FUKATA/Mojolicious-Plugin-SQLMaker-0.03.tar.gz
    gitpan-cpan-author:       FUKATA
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/SQLMaker.pm
+++ lib/Mojolicious/Plugin/SQLMaker.pm
@@ -14,11 +14,11 @@ Mojolicious::Plugin::SQLMaker - The great new Mojolicious::Plugin::SQLMaker!
 
 =head1 VERSION
 
-Version 0.01
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
